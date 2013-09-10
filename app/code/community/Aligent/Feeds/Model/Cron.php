d4b9fcd1d879e83b6e4e0cd32536ec7ae79c6ffb
Jim O'Halloran <jim@aligent.com.au>
2013-09-10 11:16:38 +0930

    System config parameter to enable/disable reindexing before export of feed.

--- app/code/community/Aligent/Feeds/Model/Cron.php
+++ app/code/community/Aligent/Feeds/Model/Cron.php
@@ -10,6 +10,8 @@
  */
 class Aligent_Feeds_Model_Cron {
 
+    const CONFIG_REINDEX = 'feeds/general/reindex';
+
     // The system config value that specifies whether the feed is enabled for this store.
     const CONFIG_ENABLED_PREFIX = 'feeds/enable/';
 
@@ -19,15 +21,16 @@ class Aligent_Feeds_Model_Cron {
      * Cron job which kicks off the feed export.
      */
     public function exportFeeds() {
-
-        Mage::getSingleton('aligent_feeds/log')->log('Starting Catalog Flat Product reindex...');
-//        $indexProcess = Mage::getSingleton('index/indexer')->getProcessByCode('catalog_product_flat');
-//        $indexProcess->reindexEverything();
-        Mage::getSingleton('aligent_feeds/log')->log('Catalog Flat Product Reindex finished!');
-        Mage::getSingleton('aligent_feeds/log')->log('Starting Catalog Flat Category reindex...');
-//        $indexProcess = Mage::getSingleton('index/indexer')->getProcessByCode('catalog_category_flat');
-//        $indexProcess->reindexEverything();
-        Mage::getSingleton('aligent_feeds/log')->log('Catalog Flat Category Reindex finished!');
+        if (Mage::getStoreConfigFlag(self::CONFIG_REINDEX)) {
+            Mage::getSingleton('aligent_feeds/log')->log('Starting Catalog Flat Product reindex...');
+            $indexProcess = Mage::getSingleton('index/indexer')->getProcessByCode('catalog_product_flat');
+            $indexProcess->reindexEverything();
+            Mage::getSingleton('aligent_feeds/log')->log('Catalog Flat Product Reindex finished!');
+            Mage::getSingleton('aligent_feeds/log')->log('Starting Catalog Flat Category reindex...');
+            $indexProcess = Mage::getSingleton('index/indexer')->getProcessByCode('catalog_category_flat');
+            $indexProcess->reindexEverything();
+            Mage::getSingleton('aligent_feeds/log')->log('Catalog Flat Category Reindex finished!');
+        }
 
         Mage::getModel('core/store')->getCollection()->walk(function($oStore) {
             $oFeeds = Mage::getConfig()->getNode(Aligent_Feeds_Model_Cron::XML_PATH_FEEDS);
