7220b7370aac7bd5d795fe42de8045794b0f70a7
Jim O'Halloran <jim@aligent.com.au>
2013-09-10 11:09:19 +0930

    Generic parameter to enable/disable any defined feed.

--- app/code/community/Aligent/Feeds/Model/Cron.php
+++ app/code/community/Aligent/Feeds/Model/Cron.php
@@ -11,7 +11,7 @@
 class Aligent_Feeds_Model_Cron {
 
     // The system config value that specifies whether the feed is enabled for this store.
-    const CONFIG_ENABLED = 'google/shopping/enable';
+    const CONFIG_ENABLED_PREFIX = 'feeds/enable/';
 
     const XML_PATH_FEEDS = 'feeds';
 
@@ -32,13 +32,14 @@ class Aligent_Feeds_Model_Cron {
         Mage::getModel('core/store')->getCollection()->walk(function($oStore) {
             $oFeeds = Mage::getConfig()->getNode(Aligent_Feeds_Model_Cron::XML_PATH_FEEDS);
             foreach($oFeeds->children() as $vFeedName => $oFeed) {
-                //if (Mage::getStoreConfigFlag(Aligent_Feeds_Model_Cron::CONFIG_ENABLED, $oStore->getId())) {
+                $vConfigName = Aligent_Feeds_Model_Cron::CONFIG_ENABLED_PREFIX.$vFeedName;
+                if (Mage::getStoreConfigFlag($vConfigName, $oStore->getId())) {
                     Mage::getModel('aligent_feeds/feed')->export($oStore, $vFeedName, $oFeed);
-                //}
+                }
             }
         });
 
-        die("Argh!");
+        die("Argh!"); // Debugging
     }
 
 }
