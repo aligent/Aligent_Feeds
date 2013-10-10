f159f28a85fb8fc6326d936214c5bba00c802d35
Jim O'Halloran <jim@aligent.com.au>
2013-10-10 16:18:55 +1030

    Add a generic strip_tags filter that can be used to remove HTML from any attribute.

---
+++ app/code/community/Aligent/Feeds/Model/Translator/Filter.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ * Performs a strip_tags on data values that might contain HTML before export.
+ *
+ * @category    Aligent
+ * @package     Aligent_Feeds
+ * @copyright   Copyright (c) 2013 Aligent Consulting
+ * @license     http://opensource.org/licenses/osl-3.0.php
+ */
+class Aligent_Feeds_Model_Translator_Filter {
+
+    /**
+     * Performs a strip_tags on data values that might contain HTML before export.
+     *
+     * @param array $aRow A flat product row
+     * @param string $vField The name of the field to filter.
+     * @param Mage_Core_Model_Store $oStore The store context we're currently exporting.
+     * @return mixed
+     */
+    public function translate($aRow, $vField, $oStore) {
+        return strip_tags($aRow[$vField]);
+    }
+}
