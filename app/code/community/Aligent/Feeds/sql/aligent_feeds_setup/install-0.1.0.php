23e6c12c08e7a1ea1a9bee67b53538b4c990bb17
William Tran <william@aligent.com.au>
2014-01-20 10:57:09 +1030

    Revert "Rename setup script for Magento Enterprise 1.10"
    This reverts commit e54515564f29fdd9b6487001d5538890f62ca3cd.

--- app/code/community/Aligent/Feeds/sql/aligent_feeds_setup/install-0.1.0.php
+++ app/code/community/Aligent/Feeds/sql/aligent_feeds_setup/install-0.1.0.php
@@ -1,5 +1,5 @@
 <?php
-$setup = new Mage_Catalog_Model_Resource_Eav_Mysql4_Setup('core_setup');
+$setup = new Mage_Catalog_Model_Resource_Setup('core_setup');
 
 $attr = array (
     'attribute_model' => NULL,
@@ -8,9 +8,9 @@ $attr = array (
     'table' => NULL,
     'frontend' => NULL,
     'input' => 'select',
-    'label' => 'Google Shopping Condition',
+    'label' => 'Google Shopping Category',
     'frontend_class' => NULL,
-    'source' => 'aligent_feeds/source_condition',
+    'source' => 'eav/entity_attribute_source_table',
     'required' => '0',
     'user_defined' => '1',
     'default' => '',
@@ -35,14 +35,27 @@ $attr = array (
     'wysiwyg_enabled' => '0',
     'used_for_promo_rules' => '0',
     'search_weight' => '1',
+    'option' =>
+    array (
+        'values' => array(),
+    ),
 );
 
-$setup->addAttribute('catalog_product', 'gshopping_condition', $attr);
+$fh = fopen(__DIR__.'/taxonomy.en-US.txt', 'r');
+// Discard the first line, it's a comment.
+$vLine = fgets($fh);
+while (!feof($fh)) {
+    $attr['option']['values'][] = trim(fgets($fh));
+}
+fclose($fh);
+
+
+$setup->addAttribute('catalog_product', 'gshopping_category', $attr);
 
 $aAttributeSetIds = $setup->getAllAttributeSetIds('catalog_product');
-$iAttributeId = $setup->getAttributeId('catalog_product', 'gshopping_condition');
+$iAttributeId = $setup->getAttributeId('catalog_product', 'gshopping_category');
 
 foreach ($aAttributeSetIds as $iAttributeSetId) {
     $iAttributeGroupId = $setup->getAttributeGroupId('catalog_product', $iAttributeSetId, 'Meta Information');
-    $setup->addAttributeToSet('catalog_product',$iAttributeSetId, $iAttributeGroupId, $iAttributeId, 6);
+    $setup->addAttributeToSet('catalog_product',$iAttributeSetId, $iAttributeGroupId, $iAttributeId, 5);
 }
