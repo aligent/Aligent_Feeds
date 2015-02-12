4c2c318759ed8f3c6a075256f1fa087c3c84ba55
Ben Turner <ben@aligent.com.au>
2015-02-12 17:20:23 +1030

    Add doc blocks to new translators

--- app/code/community/Aligent/Feeds/Model/Translator/Availability.php
+++ app/code/community/Aligent/Feeds/Model/Translator/Availability.php
@@ -1,5 +1,13 @@
 <?php
-
+/**
+ * Feed Availability Translator
+ * Class Aligent_Feeds_Model_Translator_Availability
+ *
+ * @category    Aligent
+ * @package     Aligent_Feeds
+ * @copyright   Copyright (c) 2013 Aligent Consulting
+ * @license     http://opensource.org/licenses/osl-3.0.php
+ */
 class Aligent_Feeds_Model_Translator_Availability
 {
     public function translate($aRow, $vField, $oStore) {
--- app/code/community/Aligent/Feeds/Model/Translator/Brand.php
+++ app/code/community/Aligent/Feeds/Model/Translator/Brand.php
@@ -1,11 +1,19 @@
 <?php
-
+/**
+ * Feed Brand/Manufacturer Translator
+ * Class Aligent_Feeds_Model_Translator_Brand
+ *
+ * @category    Aligent
+ * @package     Aligent_Feeds
+ * @copyright   Copyright (c) 2013 Aligent Consulting
+ * @license     http://opensource.org/licenses/osl-3.0.php
+*/
 class Aligent_Feeds_Model_Translator_Brand
 {
     public function translate($aRow) {
         $brandId = $aRow['brand_category'];
 
-        if($brandId != '') {
+        if(is_int($brandId)) {
             $oCategory = Mage::getModel('catalog/category')->load($brandId);
         }
 
