4120eb9b4630fd1bd4d89384d1d0c57299a2686a
Ben Turner <ben@aligent.com.au>
2015-02-16 14:10:25 +1030

    Make availability translator more efficient

--- app/code/community/Aligent/Feeds/Model/Translator/Availability.php
+++ app/code/community/Aligent/Feeds/Model/Translator/Availability.php
@@ -10,13 +10,7 @@
  */
 class Aligent_Feeds_Model_Translator_Availability
 {
-    public function translate($aRow, $vField, $oStore) {
-        $oProduct = Mage::getModel('catalog/product')->load($aRow['entity_id']);
-
-        if(!$oProduct){
-            return false;
-        }
-
-        return $oProduct->isInStock() ? 'In Stock' : 'Out Of Stock';
+    public function translate($aRow) {
+        return $aRow['availability'] ? 'In Stock' : 'Out Of Stock';
     }
 }
