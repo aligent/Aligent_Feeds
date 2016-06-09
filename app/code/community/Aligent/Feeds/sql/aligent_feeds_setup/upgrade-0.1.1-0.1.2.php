96a0439e968d3d64fe1fa47e69f2ddc5c091ccab
William Tran <william@aligent.com.au>
2016-06-09 14:25:09 +0930

    Added GTIN to google feed

---
+++ app/code/community/Aligent/Feeds/sql/aligent_feeds_setup/upgrade-0.1.1-0.1.2.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * Add GTIN attribute
+ * @author    William Tran <william@aligent.com.au>
+ * @copyright 2016 Aligent Consulting.
+ * @license   All Rights Reserved
+ * @link      http://www.aligent.com.au/
+ */
+
+/** @var Mage_Catalog_Model_Resource_Eav_Mysql4_Setup $installer */
+$installer = new Mage_Catalog_Model_Resource_Eav_Mysql4_Setup('core_setup');
+$installer->startSetup();
+
+$vType = 'catalog_product';
+$vGroupName = 'Meta Information';
+
+// Create Precautionary Attribute
+$vAttrCode = 'gtin';
+$vAttrName = 'GTIN';
+$aAttrData = array(
+    'label'                     => $vAttrName,
+    'global'                    => true,
+    'input'                     => 'text',
+    'is_configurable'           => false,
+    'group'                     => $vGroupName,
+    'user_defined'              => true,
+    'required'                  => false,
+    'visible'                   => '1',
+    'searchable'                => true,
+    'filterable'                => true,
+    'comparable'                => true,
+    'filterable_in_search'      => false,
+    'used_in_product_listing'   => true,
+    'is_used_for_promo_rules'   => false,
+    'visible_on_front'          => '0',
+    'is_html_allowed_on_front'  => '1'
+);
+$installer->removeAttribute($vType,$vAttrCode);
+$installer->addAttribute($vType,$vAttrCode,$aAttrData);
+
+
+$installer->endSetup();
