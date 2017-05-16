138b9686486fcd8e9018100a5c25e2563dbbb8f4
Zain ul abidin <zain@aligent.com.au>
2017-05-16 13:59:46 +0930

    price: use currency code and convert price

---
+++ app/code/community/Aligent/Feeds/Model/Translator/Translate.php
@@ -0,0 +1,30 @@
+<?php
+
+/**
+ * Performs conversion
+ *
+ * @category    Aligent
+ * @package     Aligent_Feeds
+ * @copyright   Copyright (c) 2013 Aligent Consulting
+ * @license     http://opensource.org/licenses/osl-3.0.php
+ */
+class Aligent_Feeds_Model_Translator_Translate {
+
+    /**
+     * Translate Field to price
+     *
+     * @param array $aRow A flat product row
+     * @param string $vField The name of the filed in which the price is found.
+     * @param Mage_Core_Model_Store $oStore The store context we're currently exporting.
+     * @return mixed
+     */
+    public function price($aRow, $vField, $oStore) {
+        $fPrice= $aRow['price'];
+        /**
+         * @see Mage_Core_Helper_Data::currencyByStore()  (static function)
+         */
+        $fConvertedPriced = Mage::helper('core')->currencyByStore($fPrice,$oStore,false,false);
+        return round($fConvertedPriced,2) . ' ' . $oStore->getCurrentCurrencyCode();
+    }
+
+}
