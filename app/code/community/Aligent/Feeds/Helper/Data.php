fc2b9211fea33d47be60b26a99e046393daaaa97
Michael Wylde <michael.wylde@aligent.com.au>
2017-06-14 16:06:15 +0930

    Add ability to list all categories for a product.

--- app/code/community/Aligent/Feeds/Helper/Data.php
+++ app/code/community/Aligent/Feeds/Helper/Data.php
@@ -8,4 +8,44 @@
  * @copyright   Copyright (c) 2013 Aligent Consulting
  * @license     http://opensource.org/licenses/osl-3.0.php
  */
-class Aligent_Feeds_Helper_Data extends Mage_Core_Helper_Abstract {}
+class Aligent_Feeds_Helper_Data extends Mage_Core_Helper_Abstract {
+
+    protected $_iStoreId = null;
+    protected $_oCategories = null;
+    protected $_aCategoryPaths = array();
+
+    protected function _initCategories(Mage_Core_Model_Store $oStore) {
+        $this->_oCategories = Mage::getModel('catalog/category')
+            ->getCollection()
+            ->setStoreId($oStore->getId())
+            ->addAttributeToSelect('name')
+            ->addIsActiveFilter();
+    }
+
+    public function getCategoryPath($iCategoryId, $oStore=null) {
+        if ($oStore === null) {
+            $oStore = Mage::app()->getStore();
+        }
+
+        if ($oStore->getId() !== $this->_iStoreId) {
+            $this->_aCategoryPaths = array();
+            $this->_initCategories($oStore);
+            $this->_iStoreId = $oStore->getId();
+        }
+
+        if (!array_key_exists($iCategoryId, $this->_aCategoryPaths)) {
+            $oCategory = $this->_oCategories->getItemById($iCategoryId);
+            if ($oCategory === null || $oCategory->getLevel() == 1) {
+                $this->_aCategoryPaths[$iCategoryId] = '';
+            } else {
+                $vParentCategoryPath = $this->getCategoryPath($oCategory->getParentId(), $oStore);
+                if ($vParentCategoryPath == '') {
+                    $this->_aCategoryPaths[$iCategoryId] = $oCategory->getName();
+                } else {
+                    $this->_aCategoryPaths[$iCategoryId] = $vParentCategoryPath . ' > ' . $oCategory->getName();
+                }
+            }
+        }
+        return $this->_aCategoryPaths[$iCategoryId];
+    }
+}
---
+++ app/code/community/Aligent/Feeds/Model/Translator/Categories.php
@@ -0,0 +1,32 @@
+<?php
+
+class Aligent_Feeds_Model_Translator_Categories
+{
+    /**
+     * Finds all the products categories and returns them all seperated by a pipe.
+     *
+     * @param array $aRow A flat product row
+     * @param string $vField The name of the filed in which the category id is found.
+     * @param Mage_Core_Model_Store $oStore The store context we're currently exporting.
+     * @param array $params Any additional parameters
+     * @return mixed
+     */
+    public function translate($aRow, $vField, $oStore, $params) {
+        $oProduct = Mage::getModel('catalog/product');
+        $oProduct->setId($aRow['entity_id']);
+        $cCategories = $oProduct->getResource()->getCategoryCollection($oProduct);
+
+        $aPaths = array();
+
+        foreach($cCategories as $oCategory) {
+            $aPaths[] = Mage::helper('aligent_feeds')->getCategoryPath($oCategory->getEntityId(), $oStore);
+        }
+
+        $vSeparator = '|';
+        if (isset($params['separator'])) {
+            $vSeparator = $params['separator'];
+        }
+
+        return implode($vSeparator, $aPaths);
+    }
+}
